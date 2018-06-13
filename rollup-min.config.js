9c3ee45b0edc4ac5e5eafefa96e8e3547d608988
Alexandros Dorodoulis <aldor94@gmail.com>
2018-06-13 11:48:23 +0300

    chore: Update devDependencies

--- rollup-min.config.js
+++ rollup-min.config.js
@@ -3,7 +3,7 @@ const packageJson = require('./package.json')
 
 const baseConfig = require('./rollup.config.js')
 
-baseConfig.plugins.push(uglify())
+baseConfig.plugins.push(uglify.uglify())
 baseConfig.output.file = `dist/${packageJson.name}.min.js`
 
 module.exports = baseConfig
